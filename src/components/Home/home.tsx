af44642ebd955ad49bc88575455ceb73f062c5ea
Gilson96 <grafael99@gmail.com>
2025-12-18 16:38:49 +0000

    Add search functionality

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useSearchJob } from "../../hooks/useGetQueries";
 import useScreenSize from "../../hooks/useScreenSize";
 import Jobs from "../Jobs/jobs";
 import { Button } from "../ui/button";
@@ -9,8 +11,21 @@ import {
 import { SearchIcon } from "lucide-react";
 
 const Home = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [isSearchingJob, setIsSearchingJob] = useState(false);
   const screenSize = useScreenSize();
   const mobileView = screenSize.width < 1000;
+  const {
+    error,
+    isFetching: searchFetching,
+    isLoading: searchLoading,
+    searchedJob,
+  } = useSearchJob(searchInput, isSearchingJob);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSearchingJob(true);
+  };
 
   if (mobileView) {
     return (
@@ -20,32 +35,57 @@ const Home = () => {
             <InputGroupInput
               className="italic"
               placeholder="Job title, skill or company"
+              name="searchInput"
+              onChange={(e) => {
+                (setIsSearchingJob(false), setSearchInput(e.target.value));
+              }}
             />
             <InputGroupAddon>
               <SearchIcon color="black" />
             </InputGroupAddon>
           </InputGroup>
-          <Button className="w-full font-bold">Find Jobs</Button>
+          <Button
+            onClick={() => setIsSearchingJob(true)}
+            className="w-full font-bold"
+          >
+            Find Jobs
+          </Button>
         </section>
-        <Jobs />
+        <Jobs
+          searchLoading={searchLoading}
+          searchFetching={searchFetching}
+          isSearchingJob={isSearchingJob}
+          searchedJob={searchedJob}
+        />
       </>
     );
   } else {
     return (
       <>
-        <section className="flex w-[80%] flex-col items-center justify-center gap-2 place-self-center border-b py-[3%]">
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-[80%] flex-col items-center justify-center gap-2 place-self-center border-b py-[3%]"
+        >
           <InputGroup className="h-14 pr-[2%]">
             <InputGroupInput
               className="italic"
               placeholder="Job title, skill or company"
+              onChange={(e) => {
+                (setIsSearchingJob(false), setSearchInput(e.target.value));
+              }}
             />
             <InputGroupAddon>
               <SearchIcon color="black" />
             </InputGroupAddon>
             <Button className="font-bold">Find Jobs</Button>
           </InputGroup>
-        </section>
-        <Jobs />
+        </form>
+        <Jobs
+          searchLoading={searchLoading}
+          searchFetching={searchFetching}
+          isSearchingJob={isSearchingJob}
+          searchedJob={searchedJob}
+        />
       </>
     );
   }
