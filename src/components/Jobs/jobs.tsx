7e0841968c0deaaaccde457c9ec584595be7c2a3
Gilson96 <grafael99@gmail.com>
2025-12-18 16:40:58 +0000

    Add search jobs hydratation

--- src/components/Jobs/jobs.tsx
+++ src/components/Jobs/jobs.tsx
@@ -4,15 +4,26 @@ import useScreenSize from "../../hooks/useScreenSize";
 import JobCard from "./jobCard";
 import { useState } from "react";
 import JobBigCard from "./jobBigCard";
+import type { Job } from "../../dataTypes";
 
-const Job = () => {
+const Jobs = ({
+  isSearchingJob,
+  searchedJob,
+  searchFetching,
+  searchLoading,
+}: {
+  searchFetching: boolean;
+  searchLoading: boolean;
+  isSearchingJob: boolean;
+  searchedJob?: Job[];
+}) => {
   const { jobs, error, isFetching, isLoading } = useGetAllJobs();
   const screenSize = useScreenSize();
   const mobileView = screenSize.width < 1000;
   const [jobId, setJobId] = useState(1);
 
   if (mobileView) {
-    if (isLoading) {
+    if (isLoading || searchLoading) {
       return (
         <section className="py-[2%]">
           <h2 className="font-medium">Jobs for you</h2>
@@ -21,7 +32,7 @@ const Job = () => {
       );
     }
 
-    if (isFetching) {
+    if (isFetching || searchFetching) {
       return (
         <section className="py-[2%]">
           <h2 className="pb-[5%] font-medium">Jobs for you</h2>
@@ -38,26 +49,34 @@ const Job = () => {
       <ul>
         <h2 className="pt-[2%] pb-[5%] font-medium">Jobs for you</h2>
         <li className="flex flex-col gap-3">
-          {jobs?.map((job) => (
-            <JobCard setJobId={setJobId} job={job} isFetching={false} />
-          ))}
+          {isSearchingJob
+            ? searchedJob?.map((job) => (
+                <JobCard
+                  setJobId={setJobId}
+                  job={job}
+                  isFetching={searchFetching}
+                />
+              ))
+            : jobs?.map((job) => (
+                <JobCard setJobId={setJobId} job={job} isFetching={false} />
+              ))}
         </li>
       </ul>
     );
   } else {
-    if (isLoading) {
+    if (isLoading || searchLoading) {
       return (
         <section className="w-[80%] place-self-center py-[2%]">
-          <h2 className="font-medium text-lg">Jobs for you</h2>
+          <h2 className="text-lg font-medium">Jobs for you</h2>
           <Loader2Icon className="animate mt-[3%] animate-spin place-self-center text-teal-600" />
         </section>
       );
     }
 
-    if (isFetching) {
+    if (isFetching || searchFetching) {
       return (
         <section className="w-[80%] place-self-center">
-          <h2 className="py-[2%] font-medium text-lg">Jobs for you</h2>
+          <h2 className="py-[2%] text-lg font-medium">Jobs for you</h2>
           <div className="flex h-60 w-full justify-between gap-10">
             <JobCard setJobId={setJobId} job={undefined} isFetching={true} />
             <JobCard setJobId={setJobId} job={undefined} isFetching={true} />
@@ -68,18 +87,24 @@ const Job = () => {
 
     return (
       <ul className="w-[80%] place-self-center">
-        <h2 className="pt-[2%] pb-[3%] font-medium text-lg">Jobs for you</h2>
+        <h2 className="pt-[2%] pb-[3%] text-lg font-medium">Jobs for you</h2>
         <div className="flex gap-4">
           <li className="flex w-[50%] flex-col gap-3">
-            {jobs?.map((job) => (
-              <JobCard setJobId={setJobId} job={job} isFetching={false} />
-            ))}
+            {isSearchingJob
+              ? searchedJob?.map((job) => (
+                  <JobCard setJobId={setJobId} job={job} isFetching={false} />
+                ))
+              : jobs?.map((job) => (
+                  <JobCard setJobId={setJobId} job={job} isFetching={false} />
+                ))}
           </li>
-          <JobBigCard jobId={jobId} />
+          <JobBigCard
+            jobId={isSearchingJob ? (searchedJob?.[0].job_id as number) : jobId}
+          />
         </div>
       </ul>
     );
   }
 };
 
-export default Job;
+export default Jobs;
