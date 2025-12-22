472e7371b62d2541a9804e47764ba695df4191ec
Gilson96 <grafael99@gmail.com>
2025-12-22 14:09:46 +0000

    Remove light/dark mode toggle

--- src/components/Navigator/navigator.tsx
+++ src/components/Navigator/navigator.tsx
@@ -1,20 +1,19 @@
 import { EarthIcon } from "lucide-react";
-import { ThemeToggle } from "../ui/theme-toggle";
+import { Link } from "react-router";
 
 const Navigator = () => {
   return (
     <nav className="flex h-20 w-full items-center justify-between border-b">
-      <p className="flex items-center gap-1">
+      <Link to={"/"} className="flex items-center gap-1">
         <span>
           <EarthIcon color="oklch(70.4% 0.14 182.503)" />
         </span>
         <p className="font-bold text-teal-500">Jobseekers</p>
-      </p>
+      </Link>
       <div className="flex items-center gap-2">
-        <button className="flex w-[2.5rem] items-center justify-center rounded border p-[5px] dark:bg-teal-500">
-          <ThemeToggle />
-        </button>
-        <p className="font-bold text-teal-500">Sign in</p>
+        <Link to={"/login"} className="font-bold text-teal-500">
+          Sign in
+        </Link>
       </div>
     </nav>
   );
