1390063c24d5b367d10774b7c65d556248f62c31
Gilson96 <grafael99@gmail.com>
2025-12-22 14:08:35 +0000

    Add application and user route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Navigator from "./components/Navigator/navigator";
 import Home from "./components/Home/home";
+import Application from "./components/Application/application";
+import Login from "./components/Login/login";
+import User from "./components/User/user";
 
 const App = () => {
   return (
-    <main className="h-full w-full px-[3%] py-[1%]">
+    <main className="w-full px-[3%] py-[1%]">
       <Navigator />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/application" element={<Application />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </main>
   );
