2e5c6551b50485cf5c6e8894ea1a03deaa55dc5d
Gilson96 <grafael99@gmail.com>
2025-12-23 11:58:05 +0000

    Add logout functionality into the store

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -5,16 +5,19 @@ import type { User } from '../dataTypes'
 type UserLoginTypes = {
     user: User
     setUserDetails: (user: User) => void
+    logout: () => void
 }
 
 export const useUserLoginStore = create<UserLoginTypes>()(
     persist(
         (set, get) => ({
             user: { token: '' },
-            setUserDetails: (user: User) => set(() => ({ user: user }))
+            setUserDetails: (user: User) => set(() => ({ user: user })),
+            logout: () => set(() => ({ user: {}, token: '', localStorage: localStorage.removeItem('userLogin-storage') }))
         }),
         {
             name: 'userLogin-storage',
+
         }
     ),
 )
