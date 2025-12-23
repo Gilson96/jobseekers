05dd62f95b626d743b7c62d7b3a82d97f92d19db
Gilson96 <grafael99@gmail.com>
2025-12-23 12:01:40 +0000

    Change layout if logged in

--- src/components/Navigator/navigator.tsx
+++ src/components/Navigator/navigator.tsx
@@ -1,7 +1,10 @@
-import { EarthIcon } from "lucide-react";
+import { EarthIcon, UserCircle2 } from "lucide-react";
 import { Link } from "react-router";
+import { useUserLoginStore } from "../../hooks/store";
 
 const Navigator = () => {
+  const user = useUserLoginStore((s) => s.user);
+
   return (
     <nav className="flex h-20 w-full items-center justify-between border-b">
       <Link to={"/"} className="flex items-center gap-1">
@@ -11,9 +14,15 @@ const Navigator = () => {
         <p className="font-bold text-teal-500">Jobseekers</p>
       </Link>
       <div className="flex items-center gap-2">
-        <Link to={"/login"} className="font-bold text-teal-500">
-          Sign in
-        </Link>
+        {user === undefined ? (
+          <Link to={"/login"} className="font-bold text-teal-500">
+            Sign in
+          </Link>
+        ) : (
+          <Link to={"/user"} className="font-bold text-teal-500">
+            <UserCircle2 color="oklch(70.4% 0.14 182.503)" />
+          </Link>
+        )}
       </div>
     </nav>
   );
