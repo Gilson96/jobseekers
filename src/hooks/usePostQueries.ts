0dc06ce37ce3b56cadb1431d10e01720e5cd55a0
Gilson96 <grafael99@gmail.com>
2025-12-23 17:00:21 +0000

    Add usePostSavedJobs post query

--- src/hooks/usePostQueries.ts
+++ src/hooks/usePostQueries.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import type { User } from "../dataTypes";
+import type { Job, Saved_job, User } from "../dataTypes";
 import { useUserLoginStore } from "./store";
 
 export const usePostLogin = (login: User) => {
@@ -21,4 +21,24 @@ export const usePostLogin = (login: User) => {
     });
 
   return { error, isPending, user: data, isError, isSuccess, mutate };
+};
+
+export const usePostSavedJobs = (save_job: Saved_job) => {
+  const userLogin = useUserLoginStore(s => s.user)
+  const { isPending, data, isError, isSuccess, mutate } =
+    useMutation<User>({
+      mutationFn: () => {
+        return axios
+          .post(
+            "http://jobseekers-api-c462d8f75521.herokuapp.com/api/user/saved_job",
+            { user_id: save_job.user_id, job_id: save_job.job_id },
+            { headers: { "Authorization": `Bearer ${userLogin.token}` } }
+          )
+          .then((res) => {
+            return res.data;
+          })
+      },
+    });
+
+  return { isPending, user: data, isError, isSuccess, mutate };
 };
