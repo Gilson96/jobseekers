b1fa3a6c0a40f77c384b0379b68ecd4892188127
Gilson96 <grafael99@gmail.com>
2025-12-24 14:54:00 +0000

    Add logout button

--- src/components/Navigator/navigator.tsx
+++ src/components/Navigator/navigator.tsx
@@ -8,9 +8,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
 
 const Navigator = () => {
   const user = useUserLoginStore((s) => s.user);
+  const logout = useUserLoginStore((s) => s.logout);
 
   return (
     <nav className="flex h-20 w-full items-center justify-between border-b">
@@ -21,25 +23,31 @@ const Navigator = () => {
         <p className="font-bold text-teal-500">Jobseekers</p>
       </Link>
       <div className="flex items-center gap-2">
-        {user === undefined ? (
+        {user.role === undefined ? (
           <Link to={"/login"} className="font-bold text-teal-500">
             Sign in
           </Link>
         ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <UserCircle2 className="cursor-pointer" color="oklch(70.4% 0.14 182.503)" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="relative right-[20%] w-20 min-w-0 bg-white md:right-[30%]">
-              <DropdownMenuItem>
-                <Link to={"/user"}>Profile</Link>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <Link to={"/myJobs"}>My jobs</Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <>
+            <Button onClick={() => logout()}>Logout</Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <UserCircle2
+                  className="size-8 cursor-pointer"
+                  color="oklch(70.4% 0.14 182.503)"
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="relative right-[20%] w-20 min-w-0 bg-white md:right-[30%]">
+                <DropdownMenuItem>
+                  <Link to={"/user"}>Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to={"/myJobs"}>My jobs</Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
         )}
       </div>
     </nav>
