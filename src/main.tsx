4d9bf80fbe25ff661d91f1466f137181ffb465ca
Gilson96 <grafael99@gmail.com>
2025-12-26 10:27:32 +0000

    Add react-query-devTools to debug queries

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "./hooks/useTheme-provider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools client={queryClient} />
         <BrowserRouter>
           <App />
           <Toaster />
