995d6ce0c4f128960374a491fd23fb16a057221d
Gilson96 <grafael99@gmail.com>
2025-12-28 09:26:55 +0000

    Add useDeleteUserSkills delete query

--- src/hooks/useDeleteQueries.ts
+++ src/hooks/useDeleteQueries.ts
@@ -15,6 +15,22 @@ export const useDeleteSavedJobs = (saved_job_id: number) => {
                     .then(() => { })
             },
         });
+    return { isPending, isError, isSuccess, mutate };
+};
+
+export const useDeleteUserSkills = (skills_user_id: number) => {
+    const userLogin = useUserLoginStore(s => s.user)
+    const { isPending, isError, isSuccess, mutate } =
+        useMutation({
+            mutationFn: () => {
+                return axios
+                    .delete(
+                        `http://jobseekers-api-c462d8f75521.herokuapp.com/api/user/skills_user/${skills_user_id}`,
+                        { headers: { "Authorization": `Bearer ${userLogin?.token}` } }
+                    )
+                    .then(() => { })
+            },
+        });
 
     return { isPending, isError, isSuccess, mutate };
 };
