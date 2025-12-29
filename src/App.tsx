af9c93dabb896e583093db93092ee5200c444ec2
Gilson96 <grafael99@gmail.com>
2025-12-29 15:27:36 +0000

    Add route for admin

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,34 @@ import Login from "./components/Login/login";
 import User from "./components/User/user";
 import MyJobs from "./components/User/myJobs";
 import StartPage from "./components/Home/startPage";
+import { useUserLoginStore } from "./hooks/store";
+import CompanyDashboard from "./components/Admin/companyDashboard";
 
 const App = () => {
+  const user = useUserLoginStore((s) => s.user);
+  console.log(user);
   return (
     <main className="w-full px-[3%] py-[1%]">
       <Navigator />
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/application" element={<Application />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/myJobs" element={<MyJobs />} />
+
+        {user.role === "user" ||
+          (user.name === "guest" && (
+            <>
+              <Route path="/home" element={<Home />} />
+              <Route path="/application" element={<Application />} />
+              <Route path="/user" element={<User />} />
+              <Route path="/myJobs" element={<MyJobs />} />
+            </>
+          ))}
+
+        {user.role === "admin" && (
+          <>
+            <Route path="/home" element={<CompanyDashboard />} />
+          </>
+        )}
       </Routes>
     </main>
   );
