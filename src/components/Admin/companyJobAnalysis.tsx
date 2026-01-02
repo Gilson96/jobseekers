2da9f9438241f48ec9a2a2d25d8b837b811eeac7
Gilson96 <grafael99@gmail.com>
2026-01-02 11:38:59 +0000

    Create a compenent to job analysis

---
+++ src/components/Admin/companyJobAnalysis.tsx
@@ -0,0 +1,71 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Loader2Icon } from "lucide-react";
+import { useGetApplicationJob } from "../../hooks/useGetQueries";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+
+const CompanyJobAnalysis = ({ job_id }: { job_id: number }) => {
+  const {
+    isFetching: isApplicationFetching,
+    isLoading: isApplicationLoading,
+    jobApplications,
+  } = useGetApplicationJob(job_id);
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button className="w-full" variant={"outline"}>
+          See Analysis
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogTitle>Analysis</DialogTitle>
+        <Table className="w-full">
+          <TableCaption>
+            A list of users that applied for this job.
+          </TableCaption>
+          <TableHeader className="w-full">
+            <TableRow className="w-full">
+              <TableHead>App n.</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Cv</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="w-full">
+            {isApplicationFetching || isApplicationLoading ? (
+              <section className="h-full w-full items-center justify-center">
+                <Loader2Icon className="animate animate-spin text-teal-500" />
+              </section>
+            ) : (
+              jobApplications?.map((applier) => (
+                <TableRow key={applier.application_job_id}>
+                  <TableCell className="border-r">
+                    #{applier.application_id}
+                  </TableCell>
+                  <TableCell className="border-r">{applier.name}</TableCell>
+                  <TableCell className="border-r">{applier.email}</TableCell>
+                  <TableCell>cv</TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CompanyJobAnalysis;
