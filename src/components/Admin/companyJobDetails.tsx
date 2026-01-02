678377fe8bdc4028e9bc3bf08cb9fd92949fe046
Gilson96 <grafael99@gmail.com>
2026-01-02 11:45:52 +0000

    Create a component to see company's posted job details

--- src/components/Admin/companyJobDetails.tsx
+++ src/components/Admin/companyJobDetails.tsx
@@ -1,97 +1,30 @@
 import type { Job } from "../../dataTypes";
-import { useGetApplicationJob, useGetOneJob } from "../../hooks/useGetQueries";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../ui/table";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "../ui/accordion";
-import type React from "react";
 import { Loader2Icon } from "lucide-react";
-
 const CompanyJobDetails = ({
-  job_id,
-  children,
+  job,
+  title,
+  location,
+  isJobFetching,
+  isJobLoading,
 }: {
-  job_id: number;
-  children: React.ReactNode;
+  job: Job;
+  title: string;
+  location: string;
+  isJobFetching: boolean;
+  isJobLoading: boolean;
 }) => {
-  const {
-    isFetching: isApplicationFetching,
-    isLoading: isApplicationLoading,
-    jobApplications,
-  } = useGetApplicationJob(job_id as number);
-  const {
-    isFetching: isJobFetching,
-    isLoading: isJobLoading,
-    job,
-  } = useGetOneJob(job_id);
-
   return (
     <Dialog>
-      <DialogTrigger>{children}</DialogTrigger>
+      <DialogTrigger>
+        <p className="flex w-full cursor-pointer flex-col items-start justify-start hover:underline">
+          <span className="text-left text-lg font-medium">{title}</span>
+          <span className="text-left text-sm text-neutral-600">{location}</span>
+        </p>{" "}
+      </DialogTrigger>
       <DialogContent className="flex h-[70%] w-full flex-col items-start justify-around overflow-y-auto p-[2%]">
-        <Accordion
-          className="w-full border-b pt-[2rem]"
-          type="single"
-          defaultValue="item-1"
-          collapsible
-        >
-          <AccordionItem value="item-1" className="h-full w-full">
-            <AccordionTrigger className="w-full">
-              <h3 className="w-full text-lg">Appliers:</h3>
-            </AccordionTrigger>
-            <AccordionContent className="h-full w-full">
-              <Table className="w-full">
-                <TableCaption>
-                  A list of users that applied for this job.
-                </TableCaption>
-                <TableHeader className="w-full">
-                  <TableRow className="w-full">
-                    <TableHead>App n.</TableHead>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Email</TableHead>
-                    <TableHead>Cv</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody className="w-full">
-                  {isApplicationFetching || isApplicationLoading ? (
-                    <section className="w-full h-full justify-center items-center">
-                      <Loader2Icon className="animate animate-spin text-teal-500" />
-                    </section>
-                  ) : (
-                    jobApplications?.map((applier) => (
-                      <TableRow key={applier.application_job_id}>
-                        <TableCell className="border-r">
-                          #{applier.application_id}
-                        </TableCell>
-                        <TableCell className="border-r">
-                          {applier.name}
-                        </TableCell>
-                        <TableCell className="border-r">
-                          {applier.email}
-                        </TableCell>
-                        <TableCell>cv</TableCell>
-                      </TableRow>
-                    ))
-                  )}
-                </TableBody>
-              </Table>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
         {isJobFetching || isJobLoading ? (
-          <section className="w-full h-full justify-center items-center">
+          <section className="h-full w-full items-center justify-center">
             <Loader2Icon className="animate animate-spin text-teal-500" />
           </section>
         ) : (
