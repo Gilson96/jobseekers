f8fe6ab05db6cdc4399f54cdf4d521936f01a1a0
Gilson96 <grafael99@gmail.com>
2026-01-05 18:25:35 +0000

    Remove unnecessary code

--- src/components/ui/formJobDetails.tsx
+++ src/components/ui/formJobDetails.tsx
@@ -5,86 +5,98 @@ import type { Job } from "../../dataTypes";
 const FormJobDetails = ({ action, job }: { action: string; job?: Job }) => {
   return (
     <>
-      <section className="flex w-full justify-between">
-        <section className="flex flex-col gap-2">
+      <section className="flex w-full justify-between gap-3">
+        <section className="flex flex-col gap-4">
           <div className="flex flex-col gap-2">
-            <Label htmlFor="title">Title</Label>
+            <Label className="font-normal" htmlFor="title">
+              Title
+            </Label>
             <Input
-              type="title"
+              type="text"
               name="title"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.title}
             />
           </div>
           <div className="flex flex-col gap-2">
-            <Label htmlFor="location">Location</Label>
+            <Label className="font-normal" htmlFor="location">
+              Location
+            </Label>
             <Input
-              type="location"
+              type="text"
               name="location"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.location}
             />
           </div>
           <div className="flex flex-col gap-2">
-            <Label htmlFor="pay">Pay</Label>
+            <Label className="font-normal" htmlFor="pay">
+              Pay
+            </Label>
             <Input
-              type="pay"
+              type="text"
               name="pay"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.pay}
             />
           </div>
           <div className="flex flex-col gap-2">
-            <Label htmlFor="type">type</Label>
+            <Label className="font-normal" htmlFor="type">
+              type
+            </Label>
             <Input
-              type="type"
+              type="text"
               name="type"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.type}
             />
           </div>
         </section>
-        <section className="flex flex-col gap-2">
+        <section className="flex flex-col gap-4">
           <div className="flex flex-col gap-2">
-            <Label htmlFor="about_us">About Us</Label>
-            <Input
-              type="about_us"
+            <Label className="font-normal" htmlFor="about_us">
+              About Us
+            </Label>
+            <textarea
               name="about_us"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`min-h-9 rounded border p-[2%] shadow placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.description?.about_us}
             />
           </div>
           <div className="flex flex-col gap-2">
-            <Label htmlFor="job_details">Job details</Label>
-            <Input
-              type="job_details"
+            <Label className="font-normal" htmlFor="job_details">
+              Job details
+            </Label>
+            <textarea
               name="job_details"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`min-h-9 rounded border p-[2%] shadow placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.description?.job_details}
             />
           </div>
           <div className="flex flex-col gap-2">
-            <Label htmlFor="requirements">Requirements</Label>
-            <Input
-              type="requirements"
+            <Label className="font-normal" htmlFor="requirements">
+              Requirements
+            </Label>
+            <textarea
               name="requirements"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`min-h-9 rounded border p-[2%] shadow placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.description?.requirements}
             />
           </div>
           <div className="flex flex-col gap-2">
-            <Label htmlFor="shift_pattern">Shift pattern</Label>
-            <Input
-              type="shift_pattern"
+            <Label className="font-normal" htmlFor="shift_pattern">
+              Shift pattern
+            </Label>
+            <textarea
               name="shift_pattern"
-              className={`italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
+              className={`min-h-9 rounded border p-[2%] shadow placeholder:italic ${action === "update" ? "placeholder:text-black" : "placeholder:text-neutral-400"} `}
               required={action === "post" && true}
               placeholder={job?.description?.shift_pattern}
             />
--- src/components/ui/formUserDetails.tsx
+++ src/components/ui/formUserDetails.tsx
@@ -4,9 +4,7 @@ import { Label } from "./label";
 import { Input } from "./input";
 import { ButtonGroup, ButtonGroupText } from "./button-group";
 import { Button } from "./button";
-import { useLocation } from "react-router";
 import { Loader2Icon } from "lucide-react";
-import { DialogClose } from "./dialog";
 
 type FormUserDetailsProps = {
   handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
@@ -26,41 +24,30 @@ const FormUserDetails = ({
   const [inputNumber, setInputNumber] = useState("");
   const [inputAddress, setInputAddress] = useState("");
 
-  const location = useLocation();
-  const pageLocation: string = location.pathname;
-
-  const isUserProfilePage = pageLocation === "/user";
-
-  const isInputNameChanged =
-    user?.user_id !== undefined && inputName.length > 0;
-  const isInputEmailChanged =
-    user?.user_id !== undefined && inputEmail.length > 0;
-  const isInputNumberChanged =
-    user?.user_id !== undefined && inputNumber.length > 0;
-  const isInputAddressChanged =
-    user?.user_id !== undefined && inputAddress.length > 0;
+  const isInputNameChanged = inputName.length > 0;
+  const isInputEmailChanged = inputEmail.length > 0;
+  const isInputNumberChanged = inputNumber.length > 0;
+  const isInputAddressChanged = inputAddress.length > 0;
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-      {isUserProfilePage && (
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="email">Name</Label>
-          <Input
-            type="text"
-            name="name"
-            placeholder={user?.name}
-            required={isInputNameChanged ? true : false}
-            onChange={(e) => setInputName(e.target.value)}
-          />
-        </div>
-      )}
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="email">Name</Label>
+        <Input
+          type="text"
+          name="name"
+          placeholder={user?.name}
+          required={isInputNameChanged ? true : false}
+          onChange={(e) => setInputName(e.target.value)}
+        />
+      </div>
       <div className="flex flex-col gap-2">
         <Label htmlFor="email">Email</Label>
         <Input
           type="email"
           name="email"
           placeholder={user?.email}
-          required={isInputEmailChanged ? true : false}
+          required={isInputEmailChanged || user === undefined ? true : false}
           onChange={(e) => setInputEmail(e.target.value)}
         />
       </div>
@@ -78,7 +65,7 @@ const FormUserDetails = ({
             pattern="^7.*"
             placeholder={user?.number}
             title="Number must start with 7"
-            required={isInputNumberChanged ? true : false}
+            required={isInputNumberChanged || user === undefined ? true : false}
             onChange={(e) => setInputNumber(e.target.value)}
           />
         </ButtonGroup>
@@ -91,11 +78,10 @@ const FormUserDetails = ({
           pattern=".{10,}"
           title="Address must be at least 10 characters"
           placeholder={user?.address}
-          required={isInputAddressChanged ? true : false}
+          required={isInputAddressChanged || user === undefined ? true : false}
           onChange={(e) => setInputAddress(e.target.value)}
         />
       </div>
-      {}
       <Button>
         {isPending ? (
           <Loader2Icon className="animate animate-spin text-teal-500" />
