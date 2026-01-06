da6cb723c86a9044e016858f2366abf42ef057b1
Gilson96 <grafael99@gmail.com>
2026-01-06 08:20:17 +0000

    Deployment setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
