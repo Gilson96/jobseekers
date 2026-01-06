3c979eb91feedc9767f1bfb45deead6664349e8c
Gilson96 <grafael99@gmail.com>
2026-01-06 09:59:34 +0000

    Add react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
 
@@ -14,6 +15,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools client={queryClient} />
       <BrowserRouter>
         <App />
         <Toaster />
