d2ef2ceeec23eb0ecaf8565ff9c0b8e92d885c10
Gilson96 <grafael99@gmail.com>
2026-01-06 10:37:20 +0000

    Deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/"
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
